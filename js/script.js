4ce368450ddf92014e24b1322819924707b2e0fc
Pedro Ehler <49159999+pedroehler@users.noreply.github.com>
2022-07-06 16:50:50 -0300

    função somar
    inclusão da função somar

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// let resposta = confirm("Você confirma?");
+
+// console.log("A resposta foi " + resposta);
+
+let a = 0;
+let b = 0;
+
+function somar(a, b){
+    return a + b;
+}
+
+console.log(somar(2,5));
